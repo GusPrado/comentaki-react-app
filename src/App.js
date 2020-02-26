d5e34505919ac70cd33dba2cf6b8ea466429fddd
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2020-02-26 10:48:31 -0300

    Added ContextAPI for authentication

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
 import React from 'react';
 
+import { AuthProvider } from './auth'
+
 import NewComment from './NewComment'
 import Comments from './Comments'
-
-//create account
-/*firebase
-  .auth()
-  .createUserWithEmailAndPassword('gustavo.gpprado@gmail.com', 'abc123')
-  .then(user => {
-    return user.updateProfile({
-      displayName: 'Gus Prado'
-    })
-  })*/
-
-//set username
-// firebase.auth().onAuthStateChanged(user => {
-//   if(user) {
-//     user.updateProfile({ displayName: 'Gus Prado' })
-//   }
-// })
-
+import CreateUser from './CreateUser'
 
 
 function App() {
  
   return (
-    <div>
-      <NewComment />
-      {<Comments />}
-    </div>
+    <AuthProvider>
+      <div>
+        <NewComment />
+        <Comments />
+        <CreateUser />
+      </div>
+    </AuthProvider>
   );
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app'
 import 'firebase/database'
+import 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: "AIzaSyDhAUJOVjZZe47xdomH94YKGYU3sH_8lg4",
